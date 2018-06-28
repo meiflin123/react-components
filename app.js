b1958eb5094f435e44bd8aeeffb5871be36e6d3f
meiflin123 <meiflin123@gmail.com>
2018-06-27 18:28:29 -0700

    completed

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-// TODO
+var App = () => (
+  <div>
+    <GroceryList items={['mango', 'apple', 'milk', 'bread']} />
+ 
+  </div>
+)
+
+
+var GroceryList = (props) =>(
+  <ul>
+    {props.items.map( (groceryItemName, index) => <GroceryItem item={groceryItemName} key={index}/> ) }
+  </ul>
+)
+
+class GroceryItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemhover(event){
+
+    this.setState({
+      done: !this.state.done
+    });
+
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.done ? <b>{this.props.item}</b> : this.props.item
+    }
+
+    return (
+      <li onClick={ this.onListItemhover.bind(this) }> {style.textDecoration} </li> )
+  }
+
+};
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
